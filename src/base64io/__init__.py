611ee2b90dfc4a930d2380d7fbbd50a12e829d52
EKC (Erik Cederstrand) <ekc@novozymes.com>
2018-11-02 10:38:30 +0100

    Add missing import

--- src/base64io/__init__.py
+++ src/base64io/__init__.py
@@ -23,7 +23,7 @@ LOGGER_NAME = "base64io"
 
 try:  # Python 3.5.0 and 3.5.1 have incompatible typing modules
     from types import TracebackType  # noqa pylint: disable=unused-import
-    from typing import IO, Iterable, List, Type, Optional  # noqa pylint: disable=unused-import
+    from typing import IO, Iterable, List, Type, Optional, AnyStr  # noqa pylint: disable=unused-import
 except ImportError:  # pragma: no cover
     # We only actually need these imports when running the mypy checks
     pass
